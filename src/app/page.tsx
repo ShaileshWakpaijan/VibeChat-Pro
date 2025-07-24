e2c39ef227585273dd58183f04201cc0845a05b8
Shailesh <shaileshwakpaijan@gmail.com>
2025-07-24 15:35:20 +0530

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -23,8 +24,13 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="font-light text-3xl text-amber-200">
+            Hi my name is Shailesh Wakpaijan.
+          </li>
         </ol>
-
+        <div>
+          <Button className=" bg-stone-500 cursor-pointer">Don't click me!</Button>
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
